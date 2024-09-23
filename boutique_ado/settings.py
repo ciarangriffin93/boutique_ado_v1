7d660a50fe96319bf8137694eb98b981d513b223
Ciaran Griffin <griffinciaran3@gmail.com>
2024-09-23 09:38:40 +0000

    Added add to bag functionlilty

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,10 +26,10 @@ SECRET_KEY = 'django-insecure-s0d(d-xf@#u__4ow0ab*5)xb4y82xz&@3@d^-sw6sm#&-y511=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-boutiqueado-0zalbo90fcv.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-ciarangriff-boutiqueado-o622bk5hxfu.ws.codeinstitute-ide.net']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-boutiqueado-0zalbo90fcv.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-boutiqueado-o622bk5hxfu.ws.codeinstitute-ide.net'
 ]
 
 
